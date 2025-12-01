9fa1c78431e1e2de7259e24ced463afd1e9e87d7
Thomas <tmduncan@gmail.com>
2025-11-30 22:16:45 -0500

    Remove dbg from run function

--- src/lib.rs
+++ src/lib.rs
@@ -7,8 +7,6 @@ pub mod cli;
 use crate::cli::Cli;
 
 pub fn run(cli: Cli) -> Result<(), Box<dyn Error>> {
-    dbg!(&cli);
-
     let commands = CatCommands::from_cli(&cli).modify_lines();
 
     commands.print_lines();
